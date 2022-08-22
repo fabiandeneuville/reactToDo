167adcf6756c65d8458fe87fbfb2f6d36d611352
Deneuville Fabian <deneuville.fabian@live.fr>
2022-08-22 19:46:35 +0200

    Initial commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header(){
+    return (
+        <header className="header">
+            <h1 className="header__heading">ToDo - REACT</h1>
+        </header>
+    )
+
+}
+
+export default Header;
