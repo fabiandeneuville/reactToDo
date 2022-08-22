788aee1d2d4338e80dcb51feff042854d3327d4e
Deneuville Fabian <deneuville.fabian@live.fr>
2022-08-22 22:55:52 +0200

    React toDo done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -7,23 +7,46 @@ import {useState} from 'react'
 function Form(){
 
     const [dataArr, setDataArr] = useState([
-        {txt: 'Apprendre les bases de React', id: uuidv4()},
-        {txt: 'Apprendre Redux', id: uuidv4()},
-        {txt: 'Coder 3, 4 projets avec React', id: uuidv4()}
+        // {txt: 'Apprendre les bases de React', id: uuidv4()},
     ])
+
+    const [stateInput, setStateInput] = useState()
 
     const deleteElement = (id) => {
         const filteredState = dataArr.filter((item) => item.id !== id)
         setDataArr(filteredState)
     }
 
+    const linkedInput = (e) => {
+        setStateInput(e)
+    }
+
+    const addTodo = (e) => {
+        e.preventDefault()
+
+        const newArr = [...dataArr, {
+            txt: stateInput, id: uuidv4()
+        }]
+        setDataArr(newArr)
+        setStateInput('')
+        
+    }
 
     return (
         <div>
-            <form>
+            <form
+            onSubmit={e => addTodo(e)}>
                 <label htmlFor="toDo">Choses à faire</label>
-                <input type="text" name='toDo' id="toDo"/>
-                <input type="submit" value="Ajouter"/>
+                <input 
+                onInput={e => linkedInput(e.target.value)}
+                type="text" 
+                name='toDo' 
+                id="toDo"
+                value={stateInput || ''}
+                />
+                <input 
+                type="submit" 
+                value="Ajouter"/>
             </form>
             <div className='list-container'>
                 <h2>Liste des choses à faire : </h2>
